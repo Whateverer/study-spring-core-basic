ce01a4b5491cd8a51426f371b73031fc3a0e58ed
yujin <hsjes777@gmail.com>
2023-05-11 23:35:58 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,7 +9,7 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy; // 인터페이스만 의존
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
